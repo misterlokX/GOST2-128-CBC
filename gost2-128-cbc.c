fafac078deedc9552a9feca35810a6651709d478
joannesberndt <berndtjoannes@outlook.com>
2025-09-06 03:05:48 +0200

    Update gost2-128-cbc.c

--- gost2-128-cbc.c
+++ gost2-128-cbc.c
@@ -3,8 +3,8 @@
  * Single-file utility: includes GOST2-128, SHA-256, CBC, IV generation, and I/O.
  *
  * Build:
- *   Unix/macOS: gcc gost2-128-cbc.c -o gost2file -Wall
- *   Windows (MinGW): gcc gost2-128-cbc.c -o gost2file -lbcrypt -Wall
+ *   Unix/macOS: gcc gost2-128-cbc.c -o gost2file
+ *   Windows (MinGW): gcc gost2-128-cbc.c -o gost2file -lbcrypt
  *
  * Usage:
  *   gost2file c <input_file>   -> produces <input_file>.gost2
